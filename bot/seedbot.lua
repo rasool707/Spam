ed6c7dd4b80f3bf5f85296f865ab35bcc76700bb
Tarfand-pro <tarfand-pro021205555d55d@divismail.ru>
2016-07-22 23:17:39 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
     "whitelist",
     "msg_checks"
     },
-    sudo_users = {154162986,0,0,0,tonumber(our_id)},--Sudo users
+    sudo_users = {260745261,0,0,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
